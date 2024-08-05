c9b65d7d59cec08fc2cf2c20922b3d0895d1fad9
Mohammad Haider <arzhdr@gmail.com>
2024-08-04 21:59:38 -0400

    updated env fie and cleared up folders

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD672rJdlVzMUYb0NGfX5h330z1NyMPndE",
-  authDomain: "pantry-app-2d9c8.firebaseapp.com",
-  projectId: "pantry-app-2d9c8",
-  storageBucket: "pantry-app-2d9c8.appspot.com",
-  messagingSenderId: "716508763048",
-  appId: "1:716508763048:web:91290fab10b068e5 98039d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase if it hasn't been initialized yet
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
 export { db };
